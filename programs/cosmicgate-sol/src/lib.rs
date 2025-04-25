6c696910f7783186a20e7aff3be351014ac6519c
oleh <b.dev0812@gmail.com>
2025-04-25 10:45:27 -0400

    add register node to lib

--- programs/cosmicgate-sol/src/lib.rs
+++ programs/cosmicgate-sol/src/lib.rs
@@ -1,21 +1,32 @@
 #![allow(unexpected_cfgs)]
-pub mod program_error;
-pub mod processors;
-pub mod instructions;
 pub mod account_data;
+pub mod instructions;
+pub mod processors;
+pub mod program_error;
+use crate::instructions::register_node::*;
 use anchor_lang::prelude::*;
 
 declare_id!("Bw542RoLc3equ6jTWZMp7gEJmiakoDTnLrCHc7n5QMgv");
 
 #[program]
 pub mod cosmicgate_sol {
-    use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    use super::*;
+    /**
+     * Register node and create soulbound NFT and send it to node creator
+     * # Arguments
+     *
+     * * `ctx` - The Anchor context holding the accounts
+     * * `hardware_specs` - spec of registering node
+     * * `price` - price of node for use
+     * * `uptime` - uptime of the node
+     */
+    pub fn register_node(
+        ctx: Context<RegisterNode>,
+        hardware_specs: [u8; 32],
+        price: u64,
+        uptime: u64,
+    ) -> Result<()> {
+        processors::register_node::exec(ctx, hardware_specs, price, uptime)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
