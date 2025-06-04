273618f2cc419e3c6628a88e1a78e3ad000afca1
oleh <b.dev0812@gmail.com>
2025-06-04 10:57:39 -0400

    add devent state public key

--- rust_client/src/func/config.rs
+++ rust_client/src/func/config.rs
@@ -5,4 +5,4 @@ use once_cell::sync::Lazy;
 
 
 pub static PROGRAM_ID: Lazy<Pubkey> = Lazy::new(|| Pubkey::from_str("Bw542RoLc3equ6jTWZMp7gEJmiakoDTnLrCHc7n5QMgv").unwrap());
-pub static STATE: Lazy<Pubkey> = Lazy::new(|| Pubkey::from_str("5uvXzZCSJUMjk5nLneP1MNnGvfRRRQydyW3uYF1oAQbB").unwrap());
+pub static STATE: Lazy<Pubkey> = Lazy::new(|| Pubkey::from_str("BfT2cSiemNxWiNj1kV1cwMWNEdoduCEXexo2dh9jTKaP").unwrap());   //devnet
--- rust_client/src/main.rs
+++ rust_client/src/main.rs
@@ -13,13 +13,14 @@ use func::{node::get_node_info, state::{initialize, state_info}, task::{add_task
 use func::node::register_node;
 fn main() {
     let payer = read_keypair_file("../wallets/deployer_devnet.json").unwrap();
-    let client = Client::new_with_options(Cluster::Localnet, &payer, CommitmentConfig::confirmed());
+    let client = Client::new_with_options(Cluster::Devnet, &payer, CommitmentConfig::confirmed());
     let program = client.program(*func::config::PROGRAM_ID).unwrap();
 
-    // let _ = initialize(&program, &payer);
+    let _ = initialize(&program, &payer);
+    
     // let _ = state_info(&program);
     // let _ = register_node(&program, &payer, 0);
     // let _ = get_node_info(&program, 0);
     // let _ = add_task(&program, &payer, 0, 0);
-    let _ = update_task(&program, &payer, 0, 0, 2);
+    // let _ = update_task(&program, &payer, 0, 0, 2);
 }
